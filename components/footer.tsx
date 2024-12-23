b36586ac801f24bcbf46d265688983fb8611307b
RS <rsphenomenal@gmail.com>
2024-12-23 20:41:56 +0530

    second

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="border-t">
       <div className="container mx-auto px-4 py-6 text-center text-sm text-muted-foreground">
         © {new Date().getFullYear()} Zenstreet AI Form. All rights reserved.
-          <p>Made with 💖 by <a href="https://github.com/RiteshS1" className="text-xl text-red-500">RS</a> </p>
+          <p>Made with 💖 by <a href="https://github.com/RiteshS1" target="_blank" className="text-xl text-red-500">RS</a> </p>
       </div>
     
     </footer>
